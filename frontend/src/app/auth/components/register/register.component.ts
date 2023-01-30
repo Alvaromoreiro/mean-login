2d818c54b2dc6ec7f14fd4e37f1f888e3de99e91
Moreiro <alvaromoreiro3@gmail.com>
2023-01-30 20:11:57 +0100

    feat: created register errors component

--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { registerFormInterface } from '../../models/forms.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+
+  isPasswordVisible: boolean = false;
+  isFormSubmitted: boolean = false;
+
+  usernameRegex: RegExp = /^[a-zA-Z0-9]+$/;
+  emailRegex: RegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  passwordRegex: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+  usernameFilterRegex: RegExp = /[^a-zA-Z0-9]/g;
 
   private passwordMatchValidator: ValidationErrors = (form: FormGroup) => {
     if (form.get('password')?.value !== form.get('passwordConfirm')?.value) {
@@ -16,30 +26,18 @@ export class RegisterComponent implements OnInit {
     return null;
   };
 
-  public registerForm = new FormGroup<{
-    username: FormControl<string | null>,
-    email: FormControl<string | null>,
-    password: FormControl<string | null>,
-    passwordConfirm: FormControl<string | null>
-  }>({
-    username: new FormControl('', [Validators.pattern('[a-zA-Z0-9]+'), Validators.required]),
-    email: new FormControl('', [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'), Validators.required]),
-    password: new FormControl('', [Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'), Validators.required]),
-    passwordConfirm: new FormControl('', [Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'), Validators.required])
+  public registerForm = new FormGroup<registerFormInterface>({
+    username: new FormControl('', [Validators.pattern(this.usernameRegex), Validators.required]),
+    email: new FormControl('', [Validators.pattern(this.emailRegex), Validators.required]),
+    password: new FormControl('', [Validators.pattern(this.passwordRegex), Validators.required]),
+    passwordConfirm: new FormControl('', [Validators.pattern(this.passwordRegex), Validators.required])
   }, this.passwordMatchValidator);
-
-
-  isPasswordVisible: boolean = false;
-  isFormSubmitted: boolean = false;
-
-  constructor() {}
-
-  ngOnInit(): void {
-  }
 
   onSubmit() {
     this.isFormSubmitted = true;
-    console.log(this.registerForm);
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
   }
 
 }
