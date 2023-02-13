5c8dfddcd17d014a62773e50c21de829f610878a
Moreiro <alvaromoreiro3@gmail.com>
2023-02-13 17:57:42 +0100

    delete: gitignore and modify properties.js

--- backend/config/properties.js
+++ backend/config/properties.js
@@ -1,8 +1,8 @@
-const USER = 'admin';
-const PASSWORD = '----------------';
+const USER = '';
+const PASSWORD = '';
 
 module.exports = {
-    SECRET_KEY: 'secret123',
+    SECRET_KEY: '',
     PORT: 3000,
     DB: `mongodb+srv://${USER}:${PASSWORD}@cluster0.gjui2hj.mongodb.net/?retryWrites=true&w=majority`,
 }
